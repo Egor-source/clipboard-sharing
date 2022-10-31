753455235ace3d9cfae79875ee62e162ec38f666
unknown <ego_rk@mail.ru>
2022-11-01 02:25:29 +0400

    доработал файл запуска приложения

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
 @echo off
-powershell -executionpolicy bypass -command "git status"
+powershell -executionpolicy bypass -command "git pull origin master"
+powershell -executionpolicy bypass -command "npm install"
+powershell -executionpolicy bypass -command "npm run start"
 pause
